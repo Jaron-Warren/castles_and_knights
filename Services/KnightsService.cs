42ca6f24a04c1a50cd6835526b7c9578c4a0207e
Jaron-Warren <Jarwarren@gmail.com>
2021-09-01 16:02:30 -0600

    update 1

---
+++ Services/KnightsService.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using castles_and_knights.Models;
+
+namespace castles_and_knights.Services
+{
+  public class KnightsService
+  {
+    internal IEnumerable<knight> Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal knight Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
